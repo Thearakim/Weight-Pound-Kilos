ea6275bd5da6ba65940af5defb1b667e60ac409f
Theara Kim <45727527+Thearakim@users.noreply.github.com>
2019-05-30 12:39:01 +0700

    Delete index.py

--- Hello World/index.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-if len(name)<3:
-    print("the character of name must have at least 3.")
-elif len(name)>50:
-    print("the character of name maximum is 50.")
-else:
-    print("Good name.")
